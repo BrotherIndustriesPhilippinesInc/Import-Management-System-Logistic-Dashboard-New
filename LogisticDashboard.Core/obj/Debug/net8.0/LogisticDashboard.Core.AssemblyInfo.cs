3a95cacdc0d9b93360205d8155b08002348b7637
Marc <arenga.marc.racille@gmail.com>
2025-07-31 07:50:32 +0800

    -gitignore addition

--- LogisticDashboard.Core/obj/Debug/net8.0/LogisticDashboard.Core.AssemblyInfo.cs
+++ LogisticDashboard.Core/obj/Debug/net8.0/LogisticDashboard.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogisticDashboard.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd237103aabb1fe372965381aa9e2441580e341")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe043dc66ea5b899ab7412c6dc582e9e5a93320")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogisticDashboard.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogisticDashboard.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
